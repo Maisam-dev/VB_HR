f17438067d3b72a7520709072b242eea0f32325e
MAAK PC <m.Akeel@vog.at>
2023-01-27 09:02:16 +0100

    Kommintar löschen

--- Oracdb.vb
+++ Oracdb.vb
@@ -4,16 +4,14 @@ Imports Oracle.ManagedDataAccess.Client
 Public Class Oracdb
     Private oradb As String
     Public conn
-    ' Private oradb As String = "Data Source = voglin.vog.local:1521/SAT.voglin.vog.local;Persist Security Info=True;User ID=VOG;Password=cFx2HdKmY0h#eZ58LOvKq"
-    '"Data Source = svatlihrl03.vog.local:1521/SAT.voglinz-test.wamas.com;Persist Security Info=True;User ID=wamas;Password=wamas" '
-
+    '
     Public Sub New(server As String, user As String, pass As String)
         oradb = "Data Source = " & server & ";Persist Security Info=True;User ID=" & user & ";Password=" & pass
         conn = New OracleConnection(oradb)
     End Sub
 
     '
-    ' hier muss ich teste  ob es funktioniert wird bei mehr 1000 Datensätze ?
+    ' 
     '
     Public Function insertArtikelIn(table As Hashtable) As Boolean
         Dim ret As Boolean = False
@@ -127,102 +125,6 @@ Public Class Oracdb
         Return ret
     End Function
 
-    'Allte Function was nicht mehr 1000 Datensäte gibt
-    'Public Function insertArtikelIn(table As Hashtable) As Boolean
-    '    Dim ret As Boolean = False
-    '    If table.Count < 1 Then
-    '        Return ret
-    '    End If
-    '    Dim query As String = " insert all "
-    '    Dim values As String = Nothing
-    '    Dim into As String = Nothing
-    '    Dim cmd As New OracleCommand
-    '    Dim Trans As OracleTransaction = Nothing
-
-    '    For row As Integer = 0 To table.Count - 1
-    '        values = "('" &
-    '                  table(row)("ID") & "','" &
-    '                  table(row)("BIB") & "','" &
-    '                  table(row)("ARTIKELID") & "','" &
-    '                  table(row)("TELEGRAMTYPE") & "','" &
-    '                  table(row)("ABCKLASSE") & "','" &
-    '                  "0'," &
-    '                  "sysdate," &
-    '                  "sysdate)"
-
-    '        If Not table(row)("TELEGRAMTYPE").Equals("LÖSCHUNG") Then
-    '            values = values.Substring(0, values.Length - 1) & ",'"
-    '            values += table(row)("BEZEICHNUNG") & "','" &
-    '                  table(row)("VECOLLI") & "','" &
-    '                  table(row)("COLLIPALETTE") & "','" &
-    '                  table(row)("COLLIEAN") & "','" &
-    '                  table(row)("FIFO") & "','" &
-    '                  table(row)("GEWICHT_KARTON") & "','" &
-    '                  table(row)("MHDPFLICHT") & "','" &
-    '                  table(row)("AVISOPFLICHT") & "','" &
-    '                  table(row)("CHARGENPFLICHT") & "','" &
-    '                  table(row)("HOCHREGALFAEHIG") & "','" &
-    '                  table(row)("LAGEROPTION") & "','" &
-    '                  table(row)("ABCKLASSE") & "'" &
-    '                  ")"
-    '        End If
-
-    '        into = "
-    '                 INTO ARTIKELIN ( 
-    '                                 MESSAGEID,
-    '                                 BIB,
-    '                                 ARTIKELID,
-    '                                 TELEGRAMTYPE,
-    '                                 ABCKLASSE,
-    '                                 TELEGRAMSTATE,
-    '                                 CREATED,
-    '                                 UPDATED)"
-
-    '        If Not table(row)("TELEGRAMTYPE").Equals("LÖSCHUNG") Then
-    '            into = into.Substring(0, into.Length - 1) & ","
-    '            into += "
-    '                                 BEZEICHNUNG,
-    '                                 VECOLLI ,
-    '                                 COLLIPALETTE,
-    '                                 COLLIEAN,
-    '                                 FIFO,
-    '                                 GEWICHT_KARTON,
-    '                                 MHDPFLICHT,
-    '                                 AVISOPFLICHT,
-    '                                 CHARGENPFLICHT,
-    '                                 HOCHREGALFAEHIG,
-    '                                 lageroption,
-    '                                 ABCKLASSE
-    '                                 )"
-    '        End If
-
-    '        into += " VALUES " & values
-    '        query += into
-    '    Next
-
-    '    query += "select 1 from Dual"
-
-    '    Try
-    '        conn.Open()
-    '        Trans = conn.BeginTransaction()
-    '        cmd.Connection = conn
-    '        cmd.CommandText = query
-    '        cmd.CommandType = CommandType.Text
-    '        cmd.ExecuteNonQuery()
-    '        Trans.Commit()
-    '        conn.Close()
-    '        ret = True
-    '    Catch ex As Exception
-    '        Trans.Rollback()
-    '        conn.Close()
-    '        If pt.put("Oracdb", "insertArtikelIn", query, ex.Message, Form1.loopCounter) = 2 Then
-    '            ret = True
-    '        End If
-    '    End Try
-    '    Return ret
-
-    'End Function
-    '
     '
     '
     '
@@ -292,56 +194,6 @@ Public Class Oracdb
         Return ret
     End Function
 
-
-    'Public Function getOrderIdUndMessegId(Table As Hashtable) As Hashtable
-
-    '    Dim MESSAGEIDFehle As String = " "
-    '    Dim ORDERIDFehle As String = " "
-    '    Dim MESSAGEIDOhneFehle As String = " "
-    '    Dim ORDERIDOhneFehle As String = " "
-    '    Dim OrMESSAGEIDFehle As String = " "
-    '    Dim OrMESSAGEIDOhneFehle As String = " "
-    '    Dim ErrCounter As Integer = 0
-    '    Dim CorCounter As Integer = 0
-    '    Dim messegIDTable As New Hashtable()
-
-    '    If Not IsNothing(Table) Then
-    '        For row As Integer = 0 To Table.Count - 1
-    '            If Not String.IsNullOrEmpty(Table(row)("ERRORID").ToString) Then ' containskex("key")
-    '                ErrCounter += 1
-    '                ORDERIDFehle += Table(row)("ORDERID") & ","
-
-    '                If ErrCounter < 1000 Then
-    '                    MESSAGEIDFehle += Table(row)("MESSAGEID") & ","
-    '                Else
-    '                    OrMESSAGEIDFehle += Table(row)("MESSAGEID") & ","
-    '                End If
-
-    '            Else
-    '                CorCounter += 1
-    '                ORDERIDOhneFehle += Table(row)("ORDERID") & ","
-
-    '                If CorCounter < 1000 Then
-    '                    MESSAGEIDOhneFehle += Table(row)("MESSAGEID") & ","
-    '                Else
-    '                    OrMESSAGEIDOhneFehle += Table(row)("MESSAGEID") & ","
-    '                End If
-
-    '            End If
-    '        Next
-
-    '        messegIDTable.Add("MESSAGEIDFehle", MESSAGEIDFehle.Substring(0, MESSAGEIDFehle.Length - 1))
-    '        messegIDTable.Add("ORDERIDFehle", ORDERIDFehle.Substring(0, ORDERIDFehle.Length - 1))
-    '        messegIDTable.Add("MESSAGEIDOhneFehle", MESSAGEIDOhneFehle.Substring(0, MESSAGEIDOhneFehle.Length - 1))
-    '        messegIDTable.Add("ORDERIDOhneFehle", ORDERIDOhneFehle.Substring(0, ORDERIDOhneFehle.Length - 1))
-    '        messegIDTable.Add("OrMESSAGEIDFehle", OrMESSAGEIDFehle.Substring(0, OrMESSAGEIDFehle.Length - 1))
-    '        messegIDTable.Add("OrMESSAGEIDOhneFehle", OrMESSAGEIDOhneFehle.Substring(0, OrMESSAGEIDOhneFehle.Length - 1))
-    '        Return messegIDTable
-    '    End If
-
-    'End Function
-
-
     '
     '
     '
@@ -410,7 +262,7 @@ Public Class Oracdb
 
     '
     '
-    'status_8 bei True wird prüfen ob die Spalte "PalletId" bei Fehle nicht leer, wenn ja wird die Status auf 8 erstezen statt 12
+    'status_8 bei True wird prüfen ob die Spalte "PalletId" bei Fehler nicht leer ist, wenn ja wird die Status auf 8 erstezen statt 12
     '
     Public Function getMsgIdAndOrderId(table As Hashtable, Optional status_8 As Boolean = False) As Hashtable
 
@@ -471,30 +323,6 @@ Public Class Oracdb
                         ORDERID += table(row)("ORDERID") & ","
                     End If
                 Next
-
-
-                'For row As Integer = start To end_
-
-                '    If table(row).ContainsKey("ERRORID") Then
-
-                '        If Not String.IsNullOrEmpty(table(row)("ERRORID").ToString) Then
-                '            ' prüfen die Palte id muss prüfen Table Name Move
-                '            If status_8 And isPalletID(table(row)("PALLETID").ToString) Then
-                '                MESSAGEID_8 += table(row)("MESSAGEID") & ","
-                '                ORDERID_8 += table(row)("ORDERID") & ","
-                '            Else
-                '                MESSAGEIDFehle += table(row)("MESSAGEID") & ","
-                '                ORDERIDFehle += table(row)("ORDERID") & ","
-                '            End If
-                '        Else
-                '            MESSAGEID += table(row)("MESSAGEID") & ","
-                '            ORDERID += table(row)("ORDERID") & ","
-                '        End If
-                '    Else
-                '        MESSAGEID += table(row)("MESSAGEID") & ","
-                '        ORDERID += table(row)("ORDERID") & ","
-                '    End If
-                'Next
 
                 MESSAGEIDFehle = closeVal(MESSAGEIDFehle)
                 MESSAGEID = closeVal(MESSAGEID)
